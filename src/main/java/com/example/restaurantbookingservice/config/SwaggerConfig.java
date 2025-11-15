dea12b48ab06ef5c6b2ee544199727cf8b12a24f
zhuravlevod <zhuravlevod@gmail.com>
2025-11-16 01:51:42 +0400

    fix SwaggerConfig

--- src/main/java/com/example/restaurantbookingservice/config/SwaggerConfig.java
+++ src/main/java/com/example/restaurantbookingservice/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.example.restaurantbookingservice.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +17,13 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Restaurant Booking Service API")
                         .version("1.0")
-                        .description("API for booking tables at a restaurant"));
+                        .description("API for booking tables at a restaurant"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .name("bearerAuth")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
